5f4aee84307f0118898b951b76f9f74f1f33fdb9
esbassem <esbassem@users.noreply.github.com>
2026-08-02 07:03:07 +0300

    Add automatic app update detection

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import '@/styles/index.css';
 import { AppProviders } from '@/app/providers/AppProviders';
 import { AppRouter } from '@/app/router/AppRouter';
+import { AppUpdateBanner } from '@/core/components/AppUpdateBanner';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProviders>
+      <AppUpdateBanner />
       <AppRouter />
     </AppProviders>
   </React.StrictMode>,
